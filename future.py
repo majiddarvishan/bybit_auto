ba9de39a5b059a30cbd4203e2b48a13300495c57
Majid Darvishan <majiddarvishan@outlook.com>
2023-04-11 00:19:56 +0330

    Change stop-loss

--- future.py
+++ future.py
@@ -13,33 +13,51 @@ session = HTTP(
     api_secret=API_SECRET,
 )
 
-def put_order(sybmol : str, side:str, usdt:str, leverage:str, tp:str, sl:str) -> None:
+
+def put_order(sybmol: str, side: str, usdt: str, leverage: str, tp: str, sl: str) -> str:
     resp = session.get_tickers(category="linear", symbol=sybmol)
     last_price = float(resp['result']['list'][0]['lastPrice'])
     qty = "{:.6f}".format(usdt / last_price)
 
     try:
-        resp = session.set_leverage(category="linear", symbol=sybmol, buyLeverage=leverage, sellLeverage=leverage)
+        resp = session.set_leverage(
+            category="linear", symbol=sybmol, buyLeverage=leverage, sellLeverage=leverage)
         print(resp)
     except InvalidRequestError as exp:
         print(f'{exp.status_code}, {exp.message}')
 
     try:
-        resp = session.switch_margin_mode(category="linear", symbol=sybmol, tradeMode=0, buyLeverage=leverage, sellLeverage=leverage)
+        resp = session.switch_margin_mode(
+            category="linear", symbol=sybmol, tradeMode=0, buyLeverage=leverage, sellLeverage=leverage)
         print(resp)
     except InvalidRequestError as exp:
         print(f'{exp.status_code}, {exp.message}')
 
     try:
         resp = session.place_order(category="linear",
-                                symbol=sybmol,
-                                side=side,
-                                orderType="Market",
-                                qty=qty,
-                                takeProfit=tp,
-                                stopLoss=sl)
+                                   symbol=sybmol,
+                                   side=side,
+                                   orderType="Market",
+                                   qty=qty,
+                                   takeProfit=tp,
+                                   stopLoss=sl)
+        print(resp)
+        order_id = resp["result"]["orderId"]
+        return order_id
+    except InvalidRequestError as exp:
+        print(f'{exp.status_code}, {exp.message}')
+
+    return None
+
+
+def change_stop_loss(sybmol: str, sl: str) -> None:
+    try:
+        resp = session.set_trading_stop(
+            category="linear", symbol=sybmol, stopLoss=sl, positionIdx=0)
         print(resp)
     except InvalidRequestError as exp:
         print(f'{exp.status_code}, {exp.message}')
 
-put_order("BTCUSDT", "Sell", 200, "30", "28000", "30000")
+
+# put_order("BTCUSDT", "Sell", 200, "30", "28000", "30000")
+change_stop_loss("BTCUSDT", "28500")
